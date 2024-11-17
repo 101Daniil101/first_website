e8e9a3036975ed02de22705b26dfb550a454d2ad
101Daniil101 <maklakov.daniils@yandex.ru>
2024-11-17 16:54:03 +0300

    Create an account registration and logout

--- myproject/users/forms.py
+++ myproject/users/forms.py
@@ -4,5 +4,9 @@ class LoginUserForm(forms.Form):
     username = forms.CharField(max_length=100, label="Логин")
     password = forms.CharField(max_length=100, label="Пароль")
 
+class RegisterUserForm(forms.Form):
+    username = forms.CharField(max_length=100, label="Логин")
+    password = forms.CharField(max_length=100, label="Пароль")
+    password2 = forms.CharField(max_length=100, label="Повтор пароля")
 
 
--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 from . import forms
+from django.contrib.auth.models import User
 
 def login_user(request):
     if request.method == 'POST':
@@ -11,10 +13,22 @@ def login_user(request):
             user = authenticate(request, username=cd['username'], password=cd['password'])
             if user and user.is_active:
                 login(request, user)
-                return HttpResponseRedirect('')
+                return HttpResponseRedirect(reverse('login')) #Измени
     else:
         form = forms.LoginUserForm()
     return render(request, 'users/login.html', {'form': form})
 
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('login'))
+
 def register_user(request):
-    return render(request, 'users/register.html')
+    if request.method == 'POST':
+        form = forms.RegisterUserForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = User.objects.create_user(username=cd['username'], password=cd['password'])
+            return HttpResponseRedirect(reverse('login'))
+    else:
+        form = forms.RegisterUserForm()
+    return render(request, 'users/register.html', {'form': form}) 
